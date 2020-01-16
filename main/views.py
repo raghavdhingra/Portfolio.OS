557d61d3ae0c00087e98a04ab871d230501fba0f
Raghav Dhingra <raghav.dhingra15@gmail.com>
2020-01-16 12:32:46 +0530

    Add files via upload

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse,JsonResponse
+import json
 from .models import *
+from django.core.mail import send_mail
 
 # Create your views here.
 def home(request):
@@ -17,6 +19,26 @@ def portfolio(request):
     return render(request,'portfolio.html',context)
 
 def submitForm(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        subject = request.POST.get("subject")
+        message = request.POST.get("message")
+        html_format = " Name: {} \n Subject: {} \n Email: {} \n Message: {}".format(name,subject,email,message)
+        try:
+            send_mail(
+                str(subject),
+                str(html_format),
+                'hideano123@gmail.com',
+                ['raghav.dhingra15@gmail.com'],
+                fail_silently=False,
+            )
+            return redirect("/portfolio-profile")
+
+        except Exception as e:
+            print(e)
+            return redirect("/portfolio-profile")
+
     return redirect('/')
 
 def projects(request):
